f1df2e0d0a22355babf661b49022228d5d8a33a5
James Shakespeare <james@bighuman.com>
2017-04-24 11:44:12 -0400

    first stab at clustering tracks using all echonest data points

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+const kmeans = require('node-kmeans');
 const tracks = require('./trackdata');
 
+const vectors = tracks.map((track) => {
+  const dimensions = new Array(
+    track.audio_features.danceability,
+    track.audio_features.loudness,
+    track.audio_features.speechiness,
+    track.audio_features.acousticness,
+    track.audio_features.instrumentalness,
+    track.audio_features.valence,
+    track.audio_features.tempo
+  );
+  return dimensions;
+});
 
+kmeans.clusterize(vectors, {k: 3}, (err, res) => {
+  if (err) console.error(err);
+  else {
+    res.map((cluster, clusterIdx) => {
+      console.log('\nCluster', clusterIdx);
+      cluster.clusterInd.map((trackIdx) => {
+        const track = tracks[trackIdx];
+        console.log(track.artists[0].name, '-', track.name);
+      });
+    });
+  }
+});
